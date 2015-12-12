e9b55511574d91e5f5b292c91b574667eb496bb6
Tim Strasser <tim.strasser@uzh.ch>
2015-12-13 00:19:41 +0100

    Populate My Beers, add big and small logo

--- app/src/main/java/com/beerdeer/activity/MyBeersFragment.java
+++ app/src/main/java/com/beerdeer/activity/MyBeersFragment.java
@@ -1,12 +1,30 @@
 package com.beerdeer.activity;
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.beerdeer.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MyBeersFragment extends Fragment {
 
@@ -24,6 +42,17 @@ public class MyBeersFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_my_beers, container, false);
+
+        ListView beerList = (ListView)rootView.findViewById(R.id.beer_list);
+
+        ArrayList<String> beers = new ArrayList<>();
+        beers.add("Pilsner Urquell");
+        beers.add("Budweiser Dark");
+        beers.add("Duvel");
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, beers);
+
+        beerList.setAdapter(adapter);
 
         // Inflate the layout for this fragment
         return rootView;
